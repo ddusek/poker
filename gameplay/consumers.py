b5b4209ba42cac3c6c86d1fe2abf286f6117ecc3
David Dusek <dusek.d@seznam.cz>
2020-06-13 13:50:09 +0200

    sync to async

--- gameplay/consumers.py
+++ gameplay/consumers.py
@@ -1,12 +1,5 @@
-import asyncio
 import json
-
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
-from django.contrib.auth import get_user_model
-from channels.consumer import AsyncConsumer
-from channels.db import database_sync_to_async
-
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 # class GameplayConsumer(AsyncConsumer):
 #     async def websocket_connect(self, event):
@@ -19,33 +12,31 @@ from channels.db import database_sync_to_async
 #         print('disconnect', event)
 
 
-class GameConsumer(WebsocketConsumer):
-    def connect(self):
+class GameConsumer(AsyncWebsocketConsumer):
+    async def websocket_connect(self, event):
         self.game_name = self.scope['url_route']['kwargs']['game_name']
         self.game_group_name = 'game_%s' % self.game_name
 
         # Join game group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.game_group_name,
             self.channel_name
         )
+        await self.accept()
 
-        self.accept()
-
-    def disconnect(self, close_code):
+    async def wesocket_disconnect(self, event):
         # Leave game group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.game_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        print(text_data_json)
+    async def websocket_receive(self, event):
+        text_data_json = json.loads(event['text'])
         message = text_data_json['message']
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.game_group_name,
             {
                 'type': 'chat_message',
@@ -54,10 +45,10 @@ class GameConsumer(WebsocketConsumer):
         )
 
     # Receive message from room group
-    def chat_message(self, event):
-        message = event['message']
+    async def chat_message(self, message):
+        message = message['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
