c1ac27d9162e7fe32956f2e4b7064902ea3779e4
David <dusek.d@seznam.cz>
2020-08-14 21:16:17 +0200

    setup debugger

--- manage.py
+++ manage.py
@@ -7,6 +7,16 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poker_app.settings')
+
+    from django.conf import settings
+
+    if settings.DEBUG:
+        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+            import ptvsd
+
+            ptvsd.enable_attach(address=('0.0.0.0', 8300))
+            print('Debugger attached!')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
